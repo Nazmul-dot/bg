4cb9e5c84a25e9fde45418354033f49c3566c733
unknown <nazmulhaq606845@gamil.com>
2021-01-25 17:09:57 +0600

    adding files

---
+++ bg.js
@@ -0,0 +1,17 @@
+var color1=document.querySelector("#color1");
+var color2=document.querySelector("#color2");
+var body=document.querySelector("#gradient");
+console.log(body);
+function setgradient()
+{
+    body.style.background=
+    "linear-gradient(to right, "
+    +color1.value
+    +", "
+    +color2.value
+    +")";
+}
+
+color1.addEventListener("input",setgradient);
+color2.addEventListener("input",setgradient);
+
